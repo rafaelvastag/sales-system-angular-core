9e266178417c662618b0414f00b0c5e1403d8192
Rafael Vastag <rvastag@gmail.com>
2020-12-27 17:39:31 -0300

    Template + Interpolação

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core'
+import { ClientesModule } from '../clientes/clientes.module';
 
 @Component({
   selector: 'hello',
-  template: `
-  <h1>Hello World</h1> `,
+  templateUrl: './hello.component.html'
 })
 export class HelloComponent {
+
+  nome: string;
+  clientes: Cliente[];
+
+  constructor() {
+    this.nome = 'Rafael';
+
+    let cliente1 = new Cliente('Fulano', 30);
+    let cliente2 = new Cliente('Ciclano', 45);
+
+    this.clientes = [cliente1, cliente2];
+  }
+
+}
+  class Cliente {
+
+    constructor(
+        public nome: string,
+        public idade: number) {
+    }
 
 }
